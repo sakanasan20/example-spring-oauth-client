1d6dc35ed4d35238a26f53a5c1dbb1d2712c014d
Nick <nick.chen@cmp.com.tw>
2023-07-27 15:15:33 +0800

    Add resttemplate and webclient example

--- src/main/java/tw/niq/example/ExampleSpringOauthClientApplication.java
+++ src/main/java/tw/niq/example/ExampleSpringOauthClientApplication.java
@@ -2,6 +2,12 @@ package tw.niq.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ExampleSpringOauthClientApplication {
@@ -10,4 +16,24 @@ public class ExampleSpringOauthClientApplication {
 		SpringApplication.run(ExampleSpringOauthClientApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+	@Bean
+	public WebClient webClient(ClientRegistrationRepository clientRegistrationRepository, 
+			OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository) {
+		
+		ServletOAuth2AuthorizedClientExchangeFilterFunction servletOAuth2AuthorizedClientExchangeFilterFunction = 
+				new ServletOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationRepository, 
+						oAuth2AuthorizedClientRepository);
+		
+		servletOAuth2AuthorizedClientExchangeFilterFunction.setDefaultOAuth2AuthorizedClient(true);
+		
+		return WebClient.builder()
+				.apply(servletOAuth2AuthorizedClientExchangeFilterFunction.oauth2Configuration())
+				.build();
+	}
+	
 }
---
+++ src/main/java/tw/niq/example/controller/AlbumController.java
@@ -0,0 +1,76 @@
+package tw.niq.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import tw.niq.example.model.AlbumResponseModel;
+
+@Controller
+public class AlbumController {
+	
+	@Autowired
+	OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+	
+//	@Autowired
+//	RestTemplate restTemplate;
+	
+	@Autowired
+	WebClient webClient;
+
+	@GetMapping("/albums")
+	public String getAlbums(Model model, @AuthenticationPrincipal OidcUser oidcUser) {
+		
+//		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//		
+//		OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+//		
+//		OAuth2AuthorizedClient oAuth2AuthorizedClient = oAuth2AuthorizedClientService.loadAuthorizedClient(
+//				oAuth2AuthenticationToken.getAuthorizedClientRegistrationId(), 
+//				oAuth2AuthenticationToken.getName());
+//		
+//		String jwtAccessToken = oAuth2AuthorizedClient.getAccessToken().getTokenValue();
+//		
+//		System.out.println("jwtAccessToken: " + jwtAccessToken);
+//		
+//		System.out.println(oidcUser);
+//		
+//		OidcIdToken oidcIdToken = oidcUser.getIdToken();
+//		
+//		String oidcIdTokenString = oidcIdToken.getTokenValue();
+//		
+//		System.out.println("oidcIdTokenString: " + oidcIdTokenString);
+		
+		String url = "http://localhost:8082/albums";
+
+//		HttpHeaders httpHeaders = new HttpHeaders();
+//		
+//		httpHeaders.add("Authorization", "Bearer " + jwtAccessToken);
+//		
+//		HttpEntity<List<AlbumResponseModel>> httpEntity = new HttpEntity<>(httpHeaders);
+//		
+//		ResponseEntity<List<AlbumResponseModel>> responseEntity = 
+//				restTemplate.exchange(url, HttpMethod.GET, httpEntity, new ParameterizedTypeReference<List<AlbumResponseModel>>() {});
+//		
+//		List<AlbumResponseModel> albums = responseEntity.getBody();
+		
+		List<AlbumResponseModel> albums = webClient.get()
+				.uri(url)
+				.retrieve()
+				.bodyToMono(new ParameterizedTypeReference<List<AlbumResponseModel>>() {})
+				.block();
+
+		model.addAttribute("albums", albums);
+		
+		return "albums";
+	}
+	
+}
